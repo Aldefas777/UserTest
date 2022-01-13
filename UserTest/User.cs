897fe7b9ecf5a1ce565f96d781de11716bc21d26
Aldefas777 <mkrvs6@gmail.com>
2022-01-13 18:39:54 +0300

    Обнова

--- UserTest/User.cs
+++ UserTest/User.cs
@@ -14,23 +14,5 @@ namespace UserTest
         {
             return "ID: " + UserID + " Name: " + UserName;
         }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-            User objAsUser = obj as User;
-            if (objAsUser == null) return false;
-            else return Equals(objAsUser);
-        }
-        public override int GetHashCode()
-        {
-            return UserID;
-        }
-
-        public bool Equals(User other)
-        {
-            if (other == null) return false;
-            return (this.UserID.Equals(other.UserID));
-        }
     }
 }
