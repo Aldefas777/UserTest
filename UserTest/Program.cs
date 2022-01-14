58628223c3758fcbefb1c40c551c296afd043162
Aldefas777 <mkrvs6@gmail.com>
2022-01-14 18:55:51 +0300

    Обнова

--- UserTest/Program.cs
+++ UserTest/Program.cs
@@ -9,10 +9,11 @@ public class example
 {
     public static void Main()
     {
+        IUserRepositroy repository = new UserRepasitorySQlite();
 
         int vib = 0;
 
-        Console.WriteLine("Введите нужную функцию, 1-Добавить пользоваеля, 2-Пользователей, 4-Выход");
+        Console.WriteLine("Введите нужную функцию, 1-Добавить пользоваеля, 2-Пользователей, 3-Найти пользователя, 4-Выход");
 
         while (vib != 4)
         {
@@ -20,24 +21,33 @@ public class example
             switch (vib)
             {
                 case 1:
-                    string inputText = Console.ReadLine();
-                    DataAccess.AddData(inputText);
+                    Console.WriteLine("Введите ID");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите Имя");
+                    string name = Console.ReadLine();
+                    repository.AddUser(id, name);
                     break;
                 case 2:
-                    
-
-                    foreach (User aUser in DataAccess.GetData())
+                    foreach (User aUser in repository.GetOrderedUser())
                     {
                         Console.WriteLine(aUser);
                     }
 
+                    break;
+
+                case 3:
+                    Console.WriteLine("Введите ID");
+                    int idGet = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(repository.GetUser(idGet));
+                    break;
+                case 4: 
+                    Console.WriteLine("Вы вышли из приложения");
                     break;
                 default:
                     Console.WriteLine("Такой функции нет");
                     break;
             }
         }
-        Console.ReadKey();
     }
 }
 
--- UserTest/UserRepasitorySQlite.cs
+++ UserTest/UserRepasitorySQlite.cs
@@ -9,12 +9,13 @@ using Windows.Storage;
 
 namespace UserTest
 {
-    public static class DataAccess
+    public class UserRepasitorySQlite : IUserRepositroy
     {
-        public static void AddData(string inputText)
+
+        public void AddUser(int id, string name)
         {
             string dbpath = "D:/Users.db";
-            using (SqliteConnection db = 
+            using (SqliteConnection db =
               new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
@@ -22,17 +23,17 @@ namespace UserTest
                 SqliteCommand insertCommand = new SqliteCommand();
                 insertCommand.Connection = db;
 
-                
-                insertCommand.CommandText = "INSERT INTO User VALUES (NULL, @Entry);";
-                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.CommandText = "INSERT INTO User VALUES (@Id, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Id", id);
+                insertCommand.Parameters.AddWithValue("@Entry", name);
                 insertCommand.ExecuteReader();
 
                 db.Close();
             }
-
         }
 
-        public static List<User> GetData()
+        public IEnumerable<User> GetOrderedUser()
         {
             List<User> entries = new List<User>();
 
@@ -55,6 +56,34 @@ namespace UserTest
             }
 
             return entries;
+        }
+
+        public User GetUser(int idGet)
+        {
+            List<User> entries = new List<User>();
+
+            string dbpath = "D:/Users.db";
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand();
+                selectCommand.Connection = db;
+
+
+                selectCommand.CommandText = "SELECT Name FROM User WHERE Id = @Id;";
+                selectCommand.Parameters.AddWithValue("@Id", idGet);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    Console.WriteLine(query.GetString(0));
+                }
+            }
+
+            return null;
         }
     }
 }
