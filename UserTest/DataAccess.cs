8b9a720c47a041b69462686c7c77843832e3b17e
Aldefas777 <mkrvs6@gmail.com>
2022-01-14 16:21:15 +0300

    Не рабочая

---
+++ UserTest/DataAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using Windows.Storage;
+
+namespace UserTest
+{
+    public static class DataAccess
+    {
+
+        public static void AddData(string inputText)
+        {
+            string dbpath = "D:/Торренты/UserTest/UserTest.db";
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO 'User' VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                db.Close();
+            }
+
+        }
+
+        public static List<string> GetData()
+        {
+            List<string> entries = new List<string>();
+
+            string dbpath = "D:/Торренты/UserTest/UserTest.db";
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * from 'User'", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
--- UserTest/Program.cs
+++ UserTest/Program.cs
@@ -10,6 +10,7 @@ public class example
     public static void Main()
     {
         IUserRepositroy repository = new UserRepository();
+
         int vib = 0;
 
         Console.WriteLine("Введите нужную функцию, 1-Добавить пользоваеля, 2-Вывести нужную функцию, 3-Вывести отсортированный список пользователей, 4-Выход");
@@ -19,25 +20,12 @@ public class example
             vib = Convert.ToInt32(Console.ReadLine());
             switch (vib)
             {
-                case 1:
-                    Console.Write("Введите ID ");
-                    int id = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите Имя ");
-                    string name = Console.ReadLine();
-                    repository.AddUser(id, name);
-                    break;
                 case 2:
-                    Console.Write("Наберите id нужного пользователя ");
-                    int idGet = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(repository.GetUser(idGet));
+                    string inputText = Console.ReadLine();
+                    DataAccess.AddData(inputText);
                     break;
                 case 3:
-                    foreach (User aUser in repository.GetOrderedUser())
-                    {
-                        Console.WriteLine(aUser);
-                    }
-                    break;
-                case 4: Console.WriteLine("Вы вышли из приложения");
+                    DataAccess.GetData();
                     break;
                 default:
                     Console.WriteLine("Такой функции нет");
